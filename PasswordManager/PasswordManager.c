f1278b125444135007154d927c85056ea2b2a7dc
sopy <doimpt@sopy.one>
2022-11-23 21:05:48 +0200

    password manager

---
+++ PasswordManager/PasswordManager.c
@@ -0,0 +1,16 @@
+#include "PasswordManager.h"
+
+// Dependencies
+#include <stdio.h>
+#include "../DataStore/DataStore.h"
+
+void registerPassword(char* user, char* password) {
+
+}
+void checkPassword(char* user, char* password) {
+
+}
+
+void changePassword(char* user, char* oldPassword, char* newPassword) {
+
+}
---
+++ PasswordManager/PasswordManager.h
@@ -0,0 +1,25 @@
+/*****************************************************************************\
+ !  DataStore module                                                         !
+ !  This module is used to store and retrieve data from files.               !
+ !  Copyright (C) 2022 Sopy                                                  !
+ !  FILE: PasswordManager.h                                                  !
+ *                                                                           *
+ *  AUTHOR: Sopy <contact@sopy.one>                                          *
+ *  DESCRIPTION: Header file for PasswordManager.c                           *
+ *  DEPENDENCIES:                                                            *
+ *  LICENSE: GNU General Public License v3.0                                 *
+ *                                                                           *
+ *  CREATED: 2022-11-23                                                      *
+ *  MODIFIED: 2022-11-23                                                     *
+ *  VERSION: 0.1.0                                                           *
+ ?  NOTES:                                                                   ?
+\*****************************************************************************/
+
+#ifndef PASSWORDMANAGER_PASSWORDMANAGER_H
+#define PASSWORDMANAGER_PASSWORDMANAGER_H
+
+void registerPassword(char* user, char* password);
+void checkPassword(char* user, char* password);
+void changePassword(char* user, char* oldPassword, char* newPassword);
+
+#endif //PASSWORDMANAGER_PASSWORDMANAGER_H
