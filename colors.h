fd5acfc35896715a93097f99a1b3634bc076338e
sopy <doimpt@sopy.one>
2022-11-24 19:43:25 +0200

    main.c - Creating pass.txt automatically and COLORS

---
+++ colors.h
@@ -0,0 +1,25 @@
+//
+// Created by sopy on 11/24/22.
+//
+
+#ifndef SECRETSANTA__COLORS_H_
+#define SECRETSANTA__COLORS_H_
+
+#define CLEARTERM "\e[1;1H\e[2J"
+
+#define reset "\033[0m"
+#define BOLD "\033[1m"
+#define UNDERLINE "\033[4m"
+#define BLINK "\033[5m"
+#define STRIKETHROUGH "\033[9m"
+
+#define BLACK "\033[30m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+
+#endif //SECRETSANTA__COLORS_H_
--- main.c
+++ main.c
@@ -1,33 +1,100 @@
+// dependencies
 #include <stdio.h>
 #include <malloc.h>
 #include <sys/stat.h>
-#include "DataStore/DataStore.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+// local includes
+#include "DataStore/DataStore.h"
+#include "PasswordManager/PasswordManager.h"
+#include "colors.h"
 
 // Constants
 #define CONF_FILE "./config.conf"
 
-// function prototypes
-void init(void);
+// event function prototypes
+void onInit(void);
+void run(void);
+void onExit(void);
 
+// main function
 int main() {
-  //run initialization procedure
-  init();
+  // init event
+  onInit();
 
+  // run event
+  run();
+
+  // exit event
+  onExit();
   return 0;
 }
 
-void init() {
+void onInit() {
   // setup config file
   //hacky way to define data arrays
-  DataItem defaultConfig[3] = {
+  DataItem defaultConfig[] = {
 	  {"__SIZE__", malloc(sizeof(int))},
-	  {"admin_pass", "PLEASECHANGETHIS"}};
-  *(int *) defaultConfig[0].value = 1;
+	  {"admin_pass", "YWRtaW4="}, // admin
+	  {"budget", "10"},
+	  {"currency", "EUR"}
+  };
+  *(int *) defaultConfig[0].value = 3;
 
   initData(CONF_FILE, defaultConfig);
 
   //setup folder structures
   mkdir("./data", 0777);
   mkdir("./data/participants", 0777);
+
+  // create password file
+  FILE *passFile;
+  if((passFile = fopen("./data/pass.txt", "r+")) == NULL) {
+	  // if file doesn't exist, create it
+	  passFile = fopen("./data/pass.txt", "w");
+	  if(passFile == NULL) {
+		  printf(RED "Critical error: Cannot create password file!" reset "\n");
+		  exit(1);
+	  }
+
+	  fclose(passFile);
+  }
+
+  // warn if admin password is default
+  if (strcmp(getKey("admin_pass", CONF_FILE), "YWRtaW4=") == 0) {
+	printf(YELLOW "WARNING: The admin password is still the default. Please change it!" reset "\n");
+  }
+}
+
+void run() {
+  // main loop
+  while (true) {
+	printf(CLEARTERM);
+	// print menu
+	printf("Welcome to Secret Santa!\n"
+		   "Please select an option:\n"
+		   "1. Register\n"
+		   "2. Login\n"
+		   "3. List participants\n"
+		   "\n"
+		   "9. Session Settings\n"
+		   "0. Exit\n"
+		   ">> ");
+
+	// get input
+	char input[2];
+	scanf("%s", input);
+	if (strcmp(input, "0") == 0)
+	  // exit
+	  break;
+  }
+}
+
+void onExit() {
+  // if the admin password is still the default, warn the user
+  if (strcmp(getKey("admin_pass", CONF_FILE), "YWRtaW4=") == 0) {
+	printf(YELLOW "WARNING: The admin password is still the default. Please change it!" reset "\n");
+  }
 }
