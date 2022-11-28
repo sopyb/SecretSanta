9412da6edd52c405c63982f412c7196577bfd1ae
sopy <doimpt@sopy.one>
2022-11-28 23:29:31 +0200

    Colors.h - ANSI codes (better windows compat) Over I don't know what I was u

--- colors.h
+++ colors.h
@@ -5,21 +5,38 @@
 #ifndef SECRETSANTA__COLORS_H_
 #define SECRETSANTA__COLORS_H_
 
-#define CLEARTERM "\e[1;1H\e[2J"
+// ANSI escape codes
+#define reset "\x1b[0m"
+#define BLACK "\x1b[30m"
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define YELLOW "\x1b[33m"
+#define BLUE "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN "\x1b[36m"
+#define WHITE "\x1b[37m"
 
-#define reset "\033[0m"
-#define BOLD "\033[1m"
-#define UNDERLINE "\033[4m"
-#define BLINK "\033[5m"
-#define STRIKETHROUGH "\033[9m"
+// ANSI bright colors
+#define BRIGHT_BLACK "\x1b[90m"
+#define BRIGHT_RED "\x1b[91m"
+#define BRIGHT_GREEN "\x1b[92m"
+#define BRIGHT_YELLOW "\x1b[93m"
+#define BRIGHT_BLUE "\x1b[94m"
+#define BRIGHT_MAGENTA "\x1b[95m"
+#define BRIGHT_CYAN "\x1b[96m"
+#define BRIGHT_WHITE "\x1b[97m"
 
-#define BLACK "\033[30m"
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define YELLOW "\033[33m"
-#define BLUE "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN "\033[36m"
-#define WHITE "\033[37m"
+// ANSI formatting codes
+#define BOLD "\x1b[1m"
+#define DIM "\x1b[2m"
+#define UNDERLINE "\x1b[4m"
+#define BLINK "\x1b[5m"
+#define REVERSE "\x1b[7m"
+#define HIDDEN "\x1b[8m"
+#define STRIKETHROUGH "\x1b[9m"
 
-#endif //SECRETSANTA__COLORS_H_
+// ANSI clear codes
+#define CLEARTERM "\x1b[2J"
+#define CLEARLINE "\x1b[2K"
+
+#endif //SECRETSANTA__COLORS_H
